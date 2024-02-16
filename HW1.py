44531098edea21bff0cf380c439086d5048374be
RedRocket89 <bazinga00@yahoo.com>
2024-02-15 23:01:56 -0500

    uploading excel file

--- HW1.py
+++ HW1.py
@@ -1,2 +1,10 @@
-print("This is my new ml code")
-print(" this will show up on my next push")
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+#Download and prepare the data
+mlr05 = pd.read_excel("mlr05.xlsx")
+
+
+
